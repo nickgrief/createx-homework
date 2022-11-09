da4566a20328a4a8fe9487d4b3096d71cc935bb0
Nikita Griev <wotniga@gmail.com>
2022-11-09 11:57:02 +0400

    Finish sign components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignFooterComponent } from './sign-footer/sign-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    SignFooterComponent
   ],
   imports: [
     BrowserModule,
