bc8d0a5f9dd43e7ccda2c6a0e931caf8d1db8b42
Nikita Griev <wotniga@gmail.com>
2022-11-10 17:28:44 +0400

    Add *ngFor directive to icon lists

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -10,6 +10,33 @@ export class ContactsComponent implements OnInit {
     type: 'yandex#hybrid',
   };
 
+  iconList = [
+    {
+      src: 'assets/facebook.svg',
+      alt: 'Facebook icon',
+    },
+    {
+      src: 'assets/twitter-dark.svg',
+      alt: 'Twitter icon',
+    },
+    {
+      src: 'assets/youtube.svg',
+      alt: 'Youtube icon',
+    },
+    {
+      src: 'assets/telegram.svg',
+      alt: 'Telegram icon',
+    },
+    {
+      src: 'assets/instagram.svg',
+      alt: 'Instagram icon',
+    },
+    {
+      src: 'assets/linkedin.svg',
+      alt: 'LinkedIn icon',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/sign-footer/sign-footer.component.ts
+++ src/app/sign-footer/sign-footer.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-footer',
   templateUrl: './sign-footer.component.html',
-  styleUrls: ['./sign-footer.component.scss']
+  styleUrls: ['./sign-footer.component.scss'],
 })
 export class SignFooterComponent implements OnInit {
+  iconList = [
+    {
+      src: 'assets/facebook.svg',
+      alt: 'Facebook icon',
+    },
+    {
+      src: 'assets/google.svg',
+      alt: 'Google icon',
+    },
+    {
+      src: 'assets/twitter.svg',
+      alt: 'Twitter icon',
+    },
+    {
+      src: 'assets/linkedin.svg',
+      alt: 'LinkedIn icon',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
