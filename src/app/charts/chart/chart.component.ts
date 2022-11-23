27b2bedbbaece102b1f32ddc5a52559851125440
nickgrief <wotniga@gmail.com>
2022-11-24 02:30:21 +0400

    Add closing fullscreen on escape key and disable overflow

--- src/app/charts/chart/chart.component.ts
+++ src/app/charts/chart/chart.component.ts
@@ -1,27 +1,33 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { Chart, ChartOptions, ChartConfiguration } from 'chart.js/auto';
 import { OfficeChartData } from 'src/app/services/charts-loader.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent implements AfterViewInit {
-
   @Input() chartData!: OfficeChartData;
 
   @ViewChild('myCanvas') myCanvas!: ElementRef;
 
   fullscreen = false;
 
-  public chart!: Chart<"line", string[]>;
+  public chart!: Chart<'line', string[]>;
 
   createChart(chartData: OfficeChartData) {
     this.chart = new Chart(this.myCanvas.nativeElement, {
       type: 'line', //this denotes tha type of chart
 
-      data: {// values on X-Axis
+      data: {
+        // values on X-Axis
         labels: chartData.labels,
         datasets: [...chartData.datasets],
       },
@@ -29,27 +35,41 @@ export class ChartComponent implements AfterViewInit {
       options: {
         elements: {
           point: {
-            pointStyle: "line",
+            pointStyle: 'line',
           },
           line: {
             tension: 0.25,
-          }
-        }
-      }
+          },
+        },
+      },
     });
   }
 
   chartConfig = <ChartConfiguration>{
     type: 'line',
     options: <ChartOptions>{
-      color: "red",
+      color: 'red',
+    },
+  };
+
+  toggleFullscreen() {
+    if (this.fullscreen) {
+      document.body.style.overflow = '';
+    } else {
+      document.body.style.overflow = 'hidden';
+    }
+    this.fullscreen = !this.fullscreen;
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Escape' && this.fullscreen) {
+      this.toggleFullscreen();
     }
   }
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.createChart(this.chartData);
   }
-
 }
